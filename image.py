8918cbe32c66da1c99fcf917c8fe1573b55d276b
chaetognath <jw.andrewlee@gmail.com>
2025-08-20 15:30:23 +0900

    temp save

--- image.py
+++ image.py
@@ -3,3 +3,26 @@ import skimage.io as io
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+"""
+# Read in image file
+w = io.imread('images/lena.bmp')
+
+# Display image
+plt.figure()
+io.imshow(w)
+plt.show()
+
+plt.figure()
+plt.imshow(w)
+plt.imshow(w, cmap='gray')
+plt.axis('off')
+plt.show()
+"""
+
+# Read in RGB image
+a = io.imread('images/koreaUniversity.jpg')
+plt.figure()
+io.imshow(a)
+plt.axis('off')
+plt.show()
