b358475318ee382a57f5fd550a55bfea6b3ccd8d
chaetognath <jw.andrewlee@gmail.com>
2025-08-21 11:44:04 +0900

    3.py completed

--- Jongwon/2.py
+++ Jongwon/2.py
@@ -9,11 +9,11 @@ m = np.loadtxt('images/lena.txt')
 n = np.float16(m)
 
 w = plt.imread('images/lena.bmp')
-w2 = np.uint8(w/128)*1.0
+w2 = np.int32(w%128)
 
 plt.figure()
 plt.subplot(1,2,1)
-plt.imshow(w2)
+plt.imshow(w2, cmap = 'gray')
 plt.subplot(1,2,2)
-plt.imshow(n)
+plt.imshow(n, cmap = 'gray')
 plt.show()
