52ebc59bbd8dbdb625ee4ffe00513a6cb16f0475
chaetognath <jw.andrewlee@gmail.com>
2025-08-26 16:00:25 +0900

    5.py

--- Jongwon/과제3/5.py
+++ Jongwon/과제3/5.py
@@ -7,18 +7,20 @@ image = np.float64(io.imread('images/chest.png'))
 
 # 3x3 Laplacian filter with center value = 4
 laplacian_filter = np.array([
-    [0, -1, 0],
-    [-1, 4, -1],
-    [0, -1, 0]
+    [0, 1, 0],
+    [1, -4, 1],
+    [0, 1, 0]
 ])
 
 laplacian_image = ndi.convolve(image, laplacian_filter)
 
-sharper_image = image - 0.5*laplacian_image
+sharper_image = image - laplacian_image
 
 min_val = np.min(sharper_image)
 max_val = np.max(sharper_image)
-sharper_image_rescaled = np.clip(sharper_image, min_val, max_val)
+sharper_image_rescaled = (sharper_image - min_val) / (max_val - min_val)
+sharper_image_rescaled *= 255
+
 
 plt.figure(figsize = (12,6))
 plt.subplot(1,3,1)
