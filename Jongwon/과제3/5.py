15263dca34b23f62c9ac5d6feaafdbdd919aef61
chaetognath <jw.andrewlee@gmail.com>
2025-08-26 15:23:22 +0900

    5.py

---
+++ Jongwon/과제3/5.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage.io as io
+import scipy.ndimage as ndi
+
+image = np.float64(io.imread('images/chest.png'))
+
+# 3x3 Laplacian filter with center value = 4
+laplacian_filter = np.array([
+    [0, -1, 0],
+    [-1, 4, -1],
+    [0, -1, 0]
+])
+
+laplacian_image = ndi.convolve(image, laplacian_filter)
+
+sharper_image = image - 0.5*laplacian_image
+
+min_val = np.min(sharper_image)
+max_val = np.max(sharper_image)
+sharper_image_rescaled = np.clip(sharper_image, min_val, max_val)
+
+plt.figure(figsize = (12,6))
+plt.subplot(1,3,1)
+plt.title('Original Image')
+plt.imshow(image, cmap = 'gray')
+plt.subplot(1,3,2)
+plt.title('Laplacian Image')
+plt.imshow(laplacian_image, cmap = 'gray')
+plt.subplot(1,3,3)
+plt.title('Sharper Image')
+plt.imshow(sharper_image_rescaled, cmap = 'gray')
+plt.show()
