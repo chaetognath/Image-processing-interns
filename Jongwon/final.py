e4087dffe24bdd0f950931cd5b1923a27bc42722
chaetognath <jw.andrewlee@gmail.com>
2025-09-04 15:41:15 +0900

    final done

--- Jongwon/final.py
+++ Jongwon/final.py
@@ -26,24 +26,38 @@ B_sq = np.ones((3,3), dtype = np.uint8)
 
 
 # Apply morphological opening : remove small objects
-erosion_image = morph.binary_erosion(binary_image, B_sq, mode = 'min')
-open_image = morph.binary_dilation(erosion_image, B_sq, mode= 'min')
+binary_image = morph.binary_opening(binary_image, mode = 'min')
+
 
 # Apply morphological closing : fill small holes
-dilation_image = morph.binary_dilation(open_image, footprint = B_sq, mode = 'min')
-close_image = morph.binary_erosion(dilation_image, footprint = B_sq, mode = 'min')
+binary_image = morph.binary_closing(binary_image, np.ones((7,7)), mode = 'max')
 
 # Apply Region Filling
-fill_image = ndi.binary_fill_holes(close_image, structure = np.ones((5,5))).astype(np.uint8) * 255
+binary_image = ndi.binary_fill_holes(binary_image, structure = np.ones((3,3))).astype(np.uint8) * 255
+
+# Find connected components
+lung = morph.label(binary_image, connectivity=1)
+
+lung_mask = ((lung == 4) | (lung == 3)).astype(np.uint8)
+
 
 # Find edges using morphological gradient
-dilated = morph.binary_dilation(fill_image, footprint = B_sq, mode = 'min').astype(np.uint8) * 255
-eroded = morph.binary_erosion(fill_image, footprint = B_sq, mode = 'min').astype(np.uint8) * 255
+dilated = morph.binary_dilation(lung_mask, footprint = B_sq, mode = 'min').astype(np.uint8) * 255
+eroded = morph.binary_erosion(lung_mask, footprint = B_sq, mode = 'min').astype(np.uint8) * 255
 edge_image = dilated - eroded
 
-# Add edges as red linees to the original image
-edge_colored = image.copy()
-edge_colored[edge_image == 255] = 1  # Set edge pixels to white (1 in grayscale)
+# Convert the original grayscale image to an RGB image so we can add color
+# We'll normalize it to 0-1 range for proper display with matplotlib
+image_rgb = plt.cm.gray(image)[:, :, :3] # Take only RGB channels, discard alpha if present
+
+# Overlay the red edges
+# Where edge_image is 255 (white), set the red channel to 1 (full red) and others to 0
+# Where edge_image is 0 (black), leave the original pixel color
+image_with_edges = image_rgb.copy()
+image_with_edges[edge_image == 255, 0] = 1.0  # Set Red channel to max
+image_with_edges[edge_image == 255, 1] = 0.0  # Set Green channel to min
+image_with_edges[edge_image == 255, 2] = 0.0  # Set Blue channel to min
+
 
 
 
@@ -53,6 +67,6 @@ plt.title('CT Image')
 plt.imshow(image, cmap='grey')
 plt.subplot(1,2,2)
 plt.title('Lung Segmentated')
-plt.imshow(fill_image, cmap='grey')
+plt.imshow(image_with_edges, cmap='grey')
 
 plt.show()
